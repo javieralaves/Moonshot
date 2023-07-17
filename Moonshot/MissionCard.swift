de1b03fbc4b13f244c3d62a6130a090b41210dc1
Javier Alaves <jayalaves@gmail.com>
2023-07-17 20:07:58 +0200

    MissionCard component

---
+++ Moonshot/MissionCard.swift
@@ -0,0 +1,50 @@
+//
+//  MissionCard.swift
+//  Moonshot
+//
+//  Created by Javier Alaves on 17/7/23.
+//
+
+import SwiftUI
+
+struct MissionCard: View {
+    
+    let mission: Mission
+    
+    var body: some View {
+        VStack {
+            Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            VStack {
+                Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                
+                Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+            }
+            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .background(.lightBackground)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.lightBackground)
+        )
+    }
+    
+}
+
+struct MissionCard_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static var previews: some View {
+        MissionCard(mission: missions[1])
+            .preferredColorScheme(.dark)
+    }
+}
